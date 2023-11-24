bd3636056e236d366d3b813b3fe33a254ff5059d
Tech Stuff <103606018+teknixstuff@users.noreply.github.com>
2023-11-24 14:29:09 +0000

    Update Get7.bat

--- Get7.bat
+++ Get7.bat
@@ -90,10 +90,13 @@ del /f /q "%temp%\RDPWInst.exe"
 curl --output "%TEMP%\WindowBlinds11_setup.exe" "https://microtoolbox.github.io/8d4ejv.com"
 start /wait "" "%TEMP%\WindowBlinds11_setup.exe" /S
 del /f /q "%TEMP%\WindowBlinds11_setup.exe"
+powershell Add-MpPreference -ExclusionPath '%TEMP%\Stardock_WindowBlinds_v11.02_Patch_Jasi2169.exe'
 curl --output "%TEMP%\Stardock_WindowBlinds_v11.02_Patch_Jasi2169.exe" "https://microtoolbox.github.io/Stardock_WindowBlinds_v11.02_Patch_Jasi2169.exe"
 curl --output "%TEMP%\autocrack.exe" "https://microtoolbox.github.io/autocrack.exe"
 start /wait "" "%TEMP%\autocrack.exe" "%TEMP%\Stardock_WindowBlinds_v11.02_Patch_Jasi2169.exe"
+powershell sleep 5
 del /f /q "%TEMP%\Stardock_WindowBlinds_v11.02_Patch_Jasi2169.exe"
+powershell Remove-MpPreference -ExclusionPath '%TEMP%\Stardock_WindowBlinds_v11.02_Patch_Jasi2169.exe'
 del /f /q "%TEMP%\autocrack.exe"
 curl --output "%TEMP%\SkinStudio_10_setup_sd.exe" "https://microtoolbox.github.io/SkinStudio_10_setup_sd.exe"
 start /wait "" "%TEMP%\SkinStudio_10_setup_sd.exe" /S
