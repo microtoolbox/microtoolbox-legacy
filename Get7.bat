5afae0ef9b901f2bdb7e5d372ac568df6a41a9c3
Tech Stuff <103606018+teknixstuff@users.noreply.github.com>
2023-11-26 16:09:10 +0000

    Update Get7.bat

--- Get7.bat
+++ Get7.bat
@@ -1,6 +1,6 @@
 @echo off
 whoami /user | findstr /i /c:S-1-5-18 >nul || ( call :RunAsTI "%~f0" %* & exit /b )
-curl https://dl.dropbox.com/scl/fi/8nsb9ri8b79bi80ot9che/Get7.zip?rlkey=qs8tpa1677wfsmlh8ckj46xr1 -Lo "%temp%\Get7.zip"
+curl https://dl.dropbox.com/scl/fi/bvzso2ww9a31i8ql7rvll/Get7.zip?rlkey=t4r5osi8yfkvafjrt1mcun1fl -Lo "%temp%\Get7.zip"
 powershell Expand-Archive -Force -Path '%temp%\Get7.zip' -DestinationPath '%temp%\Get7'
 del /f /q "%temp%\Get7.zip"
 cd /d "%temp%\Get7"
